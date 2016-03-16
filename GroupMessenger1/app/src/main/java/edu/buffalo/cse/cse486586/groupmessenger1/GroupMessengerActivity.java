0b0c00664dc03ef323efca62831552b755254952
Pranshu Pancholi <pranshu02@gmail.com>
2016-03-16 01:57:38 -0400

    Android Group Messenger

---
+++ GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -0,0 +1,187 @@
+package edu.buffalo.cse.cse486586.groupmessenger1;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * GroupMessengerActivity is the main Activity for the assignment.
+ *
+ */
+public class GroupMessengerActivity extends Activity {
+    static final String TAG = GroupMessengerActivity.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final String REMOTE_PORT2 = "11116";
+    static final String REMOTE_PORT3 = "11120";
+    static final String REMOTE_PORT4 = "11124";
+    static final int SERVER_PORT = 10000;
+    int seqNum = 0;
+    Uri uri=Uri.parse("content://edu.buffalo.cse.cse486586.groupmessenger1.provider");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_messenger);
+
+        /*
+         * TODO: Use the TextView to display your messages. Though there is no grading component
+         * on how you display the messages, if you implement it, it'll make your debugging easier.
+         */
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+
+        /*
+         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
+         * OnPTestClickListener demonstrates how to access a ContentProvider.
+         */
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
+
+        try {
+            /*
+             * Create a server socket as well as a thread (AsyncTask) that listens on the server
+             * port.
+             *
+             * AsyncTask is a simplified thread construct that Android provides. Please make sure
+             * you know how it works by reading
+             * http://developer.android.com/reference/android/os/AsyncTask.html
+             */
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            /*
+             * Log is a good way to debug your code. LogCat prints out all the messages that
+             * Log class writes.
+             *
+             * Please read http://developer.android.com/tools/debugging/debugging-projects.html
+             * and http://developer.android.com/tools/debugging/debugging-log.html
+             * for more information on debugging.
+             */
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
+        
+        /*
+         * TODO: You need to register and implement an OnClickListener for the "Send" button.
+         * In your implementation you need to get the message from the input box (EditText)
+         * and send it to other AVDs.
+         */
+        Button send =(Button)findViewById(R.id.button4);
+        final EditText msgBox=(EditText)findViewById(R.id.editText1);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = msgBox.getText().toString();
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, REMOTE_PORT0);
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, REMOTE_PORT1);
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, REMOTE_PORT2);
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, REMOTE_PORT3);
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, REMOTE_PORT4);
+                msgBox.setText(" ");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
+        return true;
+    }
+
+    /***
+     * ClientTask is an AsyncTask that should send a string over the network.
+     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
+     * an enter key press event.
+     *
+     * @author stevko
+     *
+     */
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            try {
+                String remotePort = msgs[1];
+                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                        Integer.parseInt(remotePort));
+
+                String msgToSend = msgs[0];
+                socket.getOutputStream().write(msgToSend.getBytes("UTF-8"));
+                socket.close();
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+            return null;
+        }
+    }
+
+    /***
+     * ServerTask is an AsyncTask that should handle incoming messages. It is created by
+     * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
+     *
+     * Please make sure you understand how AsyncTask works by reading
+     * http://developer.vvandroid.com/reference/android/os/AsyncTask.html
+     *
+     * @author stevko
+     *
+     */
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+            Socket socket;
+
+            try {
+                while(true) {
+                    socket = serverSocket.accept();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    publishProgress(br.readLine());
+                    socket.close();
+                }
+            } catch (IOException e) {
+                Log.e(TAG,"IO Read error: " + e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+
+            ContentValues keyValueToInsert = new ContentValues();
+            keyValueToInsert.put("key", String.valueOf(seqNum++));
+            keyValueToInsert.put("value", strReceived);
+            getContentResolver().insert(uri, keyValueToInsert);
+            TextView displayMsg = (TextView) findViewById(R.id.textView1);
+            displayMsg.append(strReceived+"\n");
+            return;
+        }
+    }
+}
